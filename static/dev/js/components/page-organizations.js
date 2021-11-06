6aa76d97bbda3d9cbf0e5b3423f33f5a8216e089
gezlev <gezlev@ukr.net>
2021-11-07 00:57:39 +0200

    organizations page

--- static/dev/js/components/page-organizations.js
+++ static/dev/js/components/page-organizations.js
@@ -13,23 +13,23 @@ const PageOrganizations = () => {
         let orglistMode = localStorage.getItem('orglist');
         if (orglistMode && orglistMode === 'grid') {
             container.classList.add('gridview');
-            let current = viewBtns.querySelector('.current');
-            current && current.classList.remove('current');
-            gridBtn.classList.add('current');
+            let current = viewBtns.querySelector('.active');
+            current && current.classList.remove('active');
+            gridBtn.classList.add('active');
         }
 
         viewBtns.addEventListener('click', evt => {
             let target = evt.target;
-            let current = viewBtns.querySelector('.current');
-            current && current.classList.remove('current');
+            let current = viewBtns.querySelector('.active');
+            current && current.classList.remove('active');
 
             if (listBtn.contains(target)) {
                 container.classList.remove('gridview');
-                listBtn.classList.add('current');
+                listBtn.classList.add('active');
                 localStorage.setItem('orglist', 'list');
             } else if (gridBtn.contains(target)) {
                 container.classList.add('gridview');
-                gridBtn.classList.add('current');
+                gridBtn.classList.add('active');
                 localStorage.setItem('orglist', 'grid');
             }
         }, {passive: true});
