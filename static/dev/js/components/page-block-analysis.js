054ba9465fd3a699225658d2b0e45c3dfa516b9c
gezlev <gezlev@ukr.net>
2021-11-17 17:34:09 +0200

    in progress

--- static/dev/js/components/page-block-analysis.js
+++ static/dev/js/components/page-block-analysis.js
@@ -12,24 +12,39 @@ const BlockAnalysis = () => {
     var option;
 
     const colors = ['#5470C6', '#91CC75', '#EE6666'];
+
     option = {
         color: colors,
         tooltip: {
             trigger: 'axis',
+            renderMode: 'html',
             axisPointer: {
-                type: 'cross'
+                type: 'line'
+            },
+            className: 'block-analysis--chart__tooltip',
+            appendToBody: true,
+            formatter: params => {
+                let tooltip = '<strong>' + params[0]['axisValueLabel'] + '</strong>';
+                params.forEach( el => {
+                    let className = el.seriesType === 'line' ? ' class="emptyCircle"' : '';
+                    let color = el.seriesType === 'line' ? el.borderColor : el.color;
+                    tooltip += '<div><span style="color:' + color + '"' + className + '></span> ' + el.seriesName + ' — '+ el.value + '</div>';
+                });
+                return tooltip;
             }
         },
         grid: {
-            left: '0',
-            right: '0'
+            top: 20,
+            left: 165,
+            right: 40,
+            bottom: 20,
+            containLabel: true,
+            tooltip: {
+                show: true
+            },
         },
         toolbox: {
-            feature: {
-                dataView: { show: true, readOnly: false },
-                restore: { show: true },
-                saveAsImage: { show: true }
-            }
+            show: true
         },
         legend: {
             show: false
@@ -37,86 +52,354 @@ const BlockAnalysis = () => {
         xAxis: [
             {
                 type: 'category',
-                axisTick: {
-                    alignWithLabel: true
+                axisLine: {
+                    lineStyle: {
+                        color: '#c0c0c7',
+                    }
                 },
-                // prettier-ignore
-                data: ['11-11-2021', '12-11-2021', '13-11-2021', '14-11-2021', '15-11-2021', '16-11-2021', '17-11-2021', '18-11-2021', '19-11-2021', '20-11-2021', '21-11-2021', '22-11-2021']
+                axisTick: {
+                    show: false
+                },
+                data: ['11-11-2021', '12-11-2021', '13-11-2021', '14-11-2021', '15-11-2021', '16-11-2021', '17-11-2021', '18-11-2021', '19-11-2021', '20-11-2021', '21-11-2021', '22-11-2021', '23-11-2021', '24-11-2021'],
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                name: 'Evaporation',
-                min: 0,
-                max: 250,
-                position: 'right',
-                axisLine: {
-                    show: true,
-                    lineStyle: {
-                        color: colors[0]
-                    }
-                },
-                axisLabel: {
-                    formatter: '{value} ml'
-                }
-            },
-            {
-                type: 'value',
-                name: 'Precipitation',
-                min: 0,
-                max: 250,
-                position: 'right',
-                offset: 80,
-                axisLine: {
-                    show: true,
-                    lineStyle: {
-                        color: colors[1]
-                    }
-                },
-                axisLabel: {
-                    formatter: '{value} ml'
-                }
-            },
-            {
-                type: 'value',
-                name: '温度',
-                min: 0,
-                max: 25,
                 position: 'left',
+                offset: 110,
+                boundaryGap: false,
+                name: 'Потужність (МВт)',
+                nameLocation: 'middle',
+                nameRotate: 90,
+                nameGap: 0,
+                nameTextStyle: {
+                    fontSize: 10,
+                    fontWeight: 300,
+                    color: '#c0c0c7',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    padding: [0,0,8,10]
+                },
                 axisLine: {
                     show: true,
                     lineStyle: {
-                        color: colors[2]
+                        color: '#c0c0c7'
                     }
                 },
                 axisLabel: {
-                    formatter: '{value} °C'
-                }
-            }
+                    lineHeight: 20,
+                    margin: -20,
+                    padding: 0,
+                    formatter: value => value/1000
+                },
+                axisTick: {
+                    show: false,
+                },
+                splitLine: {
+                    show: false
+                },
+            },
+            {
+                type: 'value',
+                position: 'left',
+                offset: 55,
+                boundaryGap: false,
+                name: 'Електроенергія (Вт*год)',
+                nameLocation: 'middle',
+                nameRotate: 90,
+                nameGap: 0,
+                nameTextStyle: {
+                    fontSize: 10,
+                    fontWeight: 300,
+                    color: '#c0c0c7',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    padding: [0,0,8,10]
+                },
+                axisLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#c0c0c7'
+                    }
+                },
+                axisLabel: {
+                    lineHeight: 20,
+                    margin: -24,
+                    padding: 0,
+                    formatter: value => value * 1000
+                },
+                axisTick: {
+                    show: false,
+                },
+                splitLine: {
+                    show: false
+                },
+            },
+            {
+                type: 'value',
+                position: 'left',
+                offset: 0,
+                boundaryGap: false,
+                name: 'Теплова енергія (Гкал)',
+                nameLocation: 'middle',
+                nameRotate: 90,
+                nameGap: 0,
+                nameTextStyle: {
+                    fontSize: 10,
+                    fontWeight: 300,
+                    color: '#c0c0c7',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    padding: [0,0,8,10]
+                },
+                axisLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#c0c0c7'
+                    }
+                },
+                axisLabel: {
+                    lineHeight: 20,
+                    margin: -24,
+                    padding: 0,
+                },
+                axisTick: {
+                    show: false,
+                },
+                splitLine: {
+                    show: false
+                },
+            },
+            {
+                type: 'value',
+                position: 'right',
+                offset: 60,
+                boundaryGap: false,
+                name: 'Вода (м3)', // Холодна вода
+                nameLocation: 'middle',
+                nameRotate: 90,
+                nameGap: 0,
+                nameTextStyle: {
+                    fontSize: 10,
+                    fontWeight: 300,
+                    color: '#c0c0c7',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    padding: [0,0,8,10]
+                },
+                axisLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#c0c0c7'
+                    }
+                },
+                axisLabel: {
+                    lineHeight: 20,
+                    margin: 8,
+                    padding: 0,
+                },
+                axisTick: {
+                    show: false,
+                },
+                splitLine: {
+                    lineStyle: {
+                        color: '#c0c0c7',
+                        opacity: 0.5
+                    }
+                },
+            },
+            {
+                type: 'value',
+                position: 'right',
+                offset: 0,
+                boundaryGap: false,
+                name: 'Технологічні температури (°C)', // T1
+                nameLocation: 'middle',
+                nameRotate: 90,
+                nameGap: 0,
+                nameTextStyle: {
+                    fontSize: 10,
+                    fontWeight: 300,
+                    color: '#c0c0c7',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    padding: [0,0,8,10]
+                },
+                axisLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#c0c0c7'
+                    }
+                },
+                axisLabel: {
+                    lineHeight: 20,
+                    margin: 8,
+                    padding: 0,
+                },
+                axisTick: {
+                    show: false,
+                },
+                splitLine: {
+                    show: false
+                },
+            },
+            {
+                type: 'value',
+                name: 'ГВП',
+                show: false,
+            },
+            {
+                type: 'value', // T2
+                show: false,
+            },
         ],
         series: [
             {
-                name: 'Evaporation',
-                type: 'bar',
-                data: [
-                    2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
-                ]
-            },
-            {
-                name: 'Precipitation',
-                type: 'bar',
-                yAxisIndex: 1,
-                data: [
-                    2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
-                ]
-            },
-            {
-                name: 'Temperature',
+                name: 'p',
                 type: 'line',
+                data: [0.011, 0.013, 0.014, 0.015, 0.017, 0.013, 0.012, 0.016, 0.017, 0.018, 0.019, 0.023, 0.021, 0.016],
+                yAxisIndex: 1,
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        color: '#fadf33',
+                        width: 3,
+                    }
+                },
+                itemStyle: {
+                    color: '#FFFFFF',
+                    borderWidth: 3,
+                    borderColor: '#fadf33',
+                },
+                symbol: 'circle',
+                symbolSize: 10,
+                emphasis: {
+                    itemStyle: {
+                        color: '#fadf33',
+                        borderWidth: 4,
+                        borderColor: '#FFFFFF',
+                        shadowColor: 'rgba(0, 0, 0, 0.25)',
+                        shadowBlur: 10,
+                        shadowOffsetY: 4,
+                    },
+                },
+            },
+            {
+                name: 'Електроенергія',
+                type: 'bar',
+                data: [3220, 3080, 3200, 3360, 3613, 3723, 4652, 4100, 3480, 2538, 3280, 3300, 3900, 4100],
+                barWidth: 4,
+                barMaxWidth: 6,
+                barMinWidth: 2,
+                barGap: '100%',
+                itemStyle: {
+                    color: '#ff974a',
+                    borderRadius: [3, 3, 0, 0]
+                }
+            },
+            {
+                name: 'Теплова енергія',
+                type: 'bar',
+                data: [3.22, 3.28, 3.2, 3.16, 2.9, 3.03, 4.6, 0.9, 2.4, 2.5, 2.8, 3.3, 3.6, 3.1],
+                yAxisIndex: 3,
+                barWidth: 6,
+                barMaxWidth: 4,
+                barMinWidth: 2,
+                barGap: '100%',
+                itemStyle: {
+                    color: '#679932',
+                    borderRadius: [3, 3, 0, 0]
+                }
+            },
+            {
+                name: 'Холодна вода',
+                type: 'bar',
+                data: [13, 16, 19, 31, 29, 33, 26, 29, 24, 25, 18, 33, 36, 31],
                 yAxisIndex: 2,
-                data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-            }
+                barWidth: 6,
+                barMaxWidth: 4,
+                barMinWidth: 2,
+                barGap: '100%',
+                itemStyle: {
+                    color: '#4694ff',
+                    borderRadius: [3, 3, 0, 0]
+                }
+            },
+            {
+                name: 'ГВП',
+                type: 'bar',
+                yAxisIndex: 2,
+                data: [6, 8, 9, 15, 14, 16, 13, 9, 4, 5, 8, 13, 6, 10],
+                barWidth: 6,
+                barMaxWidth: 4,
+                barMinWidth: 2,
+                barGap: '100%',
+                itemStyle: {
+                    color: '#ff4a7a',
+                    borderRadius: [3, 3, 0, 0]
+                }
+            },
+            {
+                name: 'T1',
+                type: 'line',
+                data: [189, 154, 170, 136, 148, 190, 145, 178, 112, 137, 179, 121, 192, 160],
+                yAxisIndex: 4,
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        color: '#679932',
+                        width: 3,
+                    }
+                },
+                itemStyle: {
+                    color: '#FFFFFF',
+                    borderWidth: 3,
+                    borderColor: '#679932',
+                },
+                symbol: 'circle',
+                symbolSize: 10,
+                emphasis: {
+                    itemStyle: {
+                        color: '#679932',
+                        borderWidth: 4,
+                        borderColor: '#FFFFFF',
+                        shadowColor: 'rgba(0, 0, 0, 0.25)',
+                        shadowBlur: 10,
+                        shadowOffsetY: 4,
+                    },
+                },
+            },
+            {
+                name: 'T2',
+                type: 'line',
+                data: [53, 46, 49, 51, 48, 52, 49, 52, 48, 53, 52, 46, 47, 49],
+                yAxisIndex: 4,
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        color: '#8cd31a',
+                        width: 3,
+                    }
+                },
+                itemStyle: {
+                    color: '#FFFFFF',
+                    borderWidth: 3,
+                    borderColor: '#8cd31a',
+                },
+                symbol: 'circle',
+                symbolSize: 10,
+                emphasis: {
+                    itemStyle: {
+                        color: '#8cd31a',
+                        borderWidth: 4,
+                        borderColor: '#FFFFFF',
+                        shadowColor: 'rgba(0, 0, 0, 0.25)',
+                        shadowBlur: 10,
+                        shadowOffsetY: 4,
+                    },
+                },
+            },
         ]
     };
 
